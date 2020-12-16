7d12493cf90e127cd2b0ca9ba1845243877db973
ayokunle <ayokunle.fatokimi@decagon.dev>
2020-12-16 11:06:12 +0100

    add instruction to playground

---
+++ traditionalBank.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+/*
+ The task is to model a Traditional BANK having a Bank account, Bank employee, a Bank customer object using OOP.
+
+ Bank Account : A financial account maintained by the bank. For our implementation;
+ We would be limiting bank account type to Savings Account Current Account
+
+ Bank Customer: An individual that has owns an account with the bank.
+ */
